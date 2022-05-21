3863641403ae1afc9205102589b06d040b32a942
Natalia Sharshakova <sharshanatty@gmail.com>
2022-05-21 20:01:25 +0300

    Рендеринг JSX

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+
+    function handleMessage(message: number) {
+        setMessage(message);
+    }
+
+    useEffect(() => {
+        setMessage(-1);
+        subscribe(props.sourceId, handleMessage);
+        return () => {
+            unsubscribe(props.sourceId, handleMessage);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
